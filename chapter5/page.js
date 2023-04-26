33fe78d2bf74ae8274ac80ab87f5e64bacf090d9
lettie-zulu <129884603+lettie-zulu@users.noreply.github.com>
2023-04-26 14:15:38 +0200

    Add files via upload

---
+++ chapter5/page.js
@@ -0,0 +1,35 @@
+// var message = "Hello World";
+// alert(message);
+
+// var name = prompt("what's your name?");
+// console.log("Hello " + name);
+
+// var likesCats = confirm("Do you like cats?");
+// if (likesCats) {
+//     console.log("You're a cool cat!");
+// } else {
+//     console.log("Yeah, that's fine. You're still cool!")
+// }
+
+// alert("javaScript is awesome!");
+// var words = [
+//     "javascript",
+//     "monkey",
+//     "amazing",
+//     "pancake"
+//     ];
+// var word = words[Math.floor(Math.random() * words.length)];
+
+// var answerArray = [];
+// for (var i = 0; i < word.length; i++) {
+//     answerArray[i] = "_";
+// }
+
+// var remainingLetters = word.length;
+
+// while (remainingLetters > 0) {
+//     // Game code goes here
+//     // Show the player their progress
+//     // Take input from the player
+//     // Update answerArray and remainingLetters for every correct guess
+// }
